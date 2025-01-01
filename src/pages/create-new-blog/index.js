981006889482bbaa362741d29f903d12859dc6cb
hishmatrai <hishmatrai.2018@gmail.com>
2025-01-01 19:01:37 +0500

    update

--- src/pages/create-new-blog/index.js
+++ src/pages/create-new-blog/index.js
@@ -7,7 +7,20 @@ import TextField from "@mui/material/TextField";
 import { toast } from "react-toastify";
 import moment from "moment/moment";
 import { v4 as uuidv4 } from "uuid";
+import Box from "@mui/material/Box";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import LinearProgress from "@mui/material/LinearProgress";
+import Typography from "@mui/material/Typography";
+import ReactPlayer from "react-player";
+import { getDatabase, push, ref as databaseRef } from "firebase/database";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+
+import {
+  getStorage,
+  uploadBytesResumable,
+  getDownloadURL,
+  ref as storageRef,
+} from "firebase/storage";
 const VisuallyHiddenInput = styled("input")({
   clip: "rect(0 0 0 0)",
   clipPath: "inset(50%)",
@@ -19,13 +32,32 @@ const VisuallyHiddenInput = styled("input")({
   whiteSpace: "nowrap",
   width: 1,
 });
-
+function LinearProgressWithLabel(props) {
+  return (
+    <Box sx={{ display: "flex", alignItems: "center" }}>
+      <Box sx={{ width: "100%", mr: 1 }}>
+        <LinearProgress variant="determinate" {...props} />
+      </Box>
+      <Box sx={{ minWidth: 35 }}>
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+          {`${Math.round(props.value)}%`}
+        </Typography>
+      </Box>
+    </Box>
+  );
+}
 const CreateNewBlog = () => {
   const auth = getAuth();
-  const [fileURl, setFileUrl] = useState("ffd");
+  const storage = getStorage();
+  const realTime = getDatabase();
+  const firestore = getFirestore();
+  const [fileURl, setFileUrl] = useState("");
   const [title, setTitle] = useState("");
   const [details, setDetails] = useState("");
   const [uid, setUid] = useState(null);
+  const [progress, setProgress] = React.useState(0);
+  const [profileUploadStart, setProfileUploadStart] = useState(false);
+  const [fileType, setFileType] = useState("");
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
@@ -34,7 +66,7 @@ const CreateNewBlog = () => {
       }
     });
   }, []);
-  const createHandler = () => {
+  const createHandler = async () => {
     if (fileURl === "") {
       toast("File required!", { type: "error" });
     } else if (title === "") {
@@ -55,10 +87,46 @@ const CreateNewBlog = () => {
         share: 0,
         createdDate: moment().format(),
         uid: uid,
+        fileType: fileType,
       };
       console.log("newBlog", newBlog);
+      push(databaseRef(realTime, "blogs/"), newBlog);
+      await addDoc(collection(firestore, "blogs"), newBlog);
+      setFileType("");
+      setFileUrl("");
+      setTitle("");
+      setDetails("")
       toast("Successfully created new post", { type: "success" });
     }
+  };
+
+  // file upload
+  const fileUploadHandler = (event) => {
+    let file = event.target.files[0];
+    console.log(file);
+    setProfileUploadStart(true);
+    const profileStorageRef = storageRef(storage, `blog-files/${uuidv4()}`);
+    const uploadTask = uploadBytesResumable(profileStorageRef, file);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const uploadProgress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + uploadProgress + "% done");
+        setProgress(uploadProgress);
+      },
+      (error) => {
+        // Handle unsuccessful uploads
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+          console.log("File available at", downloadURL);
+          setFileUrl(downloadURL);
+          setFileType(file.type);
+          setProfileUploadStart(false);
+        });
+      }
+    );
   };
   return (
     <Layout>
@@ -75,9 +143,28 @@ const CreateNewBlog = () => {
         <VisuallyHiddenInput
           type="file"
           // onChange={(event) => console.log(event.target.files)}
-          // onChange={(event) => uploadProfileImage(event)}
+          onChange={(event) => fileUploadHandler(event)}
         />
       </Button>
+      {profileUploadStart && (
+        <Box sx={{ width: "100%" }}>
+          <LinearProgressWithLabel value={progress} />
+        </Box>
+      )}
+      {fileURl !== "" && (
+        <div>
+          {fileType === "video/mp4" ? (
+            <ReactPlayer
+              url="https://firebasestorage.googleapis.com/v0/b/web-development-batch-11.firebasestorage.app/o/blog-files%2F1dccd5a3-be8b-4676-992d-3c50974441b0?alt=media&token=a8635534-268a-450f-bc8e-ca27822cf5d8"
+              controls={true}
+              width={250}
+              height={250}
+            />
+          ) : (
+            <img src={fileURl} width={250} height={250} />
+          )}
+        </div>
+      )}
       <br /> <br />
       <TextField
         id="outlined-basic"
